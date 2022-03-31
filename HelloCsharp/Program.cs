cab57000ebc67dd666c7b222a33687b6c099329d
Giuseppe Russo <russogiuseppe412@gmail.com>
2022-03-31 13:18:08 +0200

    Programma finito

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -6,8 +6,21 @@ namespace HelloCsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            string[] mesi = { "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre" };
+            Console.WriteLine("Benvenuti!");
+
+            for (int i = 0; i < mesi.Length; i++)
+            {
+                Console.WriteLine(mesi[i]);
+            }
+
+            Console.Write("Qual'è il tuo mese preferito? ");
+            string mese = Console.ReadLine();
+
+            Console.WriteLine("Questo è il tuo mese preferito: " + mese);
+            Console.WriteLine("Grazie!");
+
+
         }
     }
 }
